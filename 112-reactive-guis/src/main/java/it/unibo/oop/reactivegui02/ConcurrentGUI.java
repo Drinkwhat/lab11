c360c5024b849e7db7fbfe759dd6a72cf816ded5
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-12-02 12:05:47 +0100

    Refactor ConcurrentGUI to enhance counter functionality and update display

--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -1,7 +1,15 @@
 package it.unibo.oop.reactivegui02;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import it.unibo.oop.JFrameUtil;
+
 import java.io.Serial;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Second example of reactive GUI.
@@ -10,4 +18,79 @@ public final class ConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private final JLabel display = new JLabel();
+
+    /**
+     * Builds a new CGUI.
+     */
+    public ConcurrentGUI() {
+        super();
+        JFrameUtil.dimensionJFrame(this);
+
+        final JPanel panel = new JPanel();
+        final JButton up = new JButton("up");
+        final JButton down = new JButton("down");
+        final JButton stop = new JButton("stop");
+
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+
+        final Counter counter = new Counter();
+        new Thread(counter).start();
+
+        up.addActionListener(e -> counter.increment());
+        down.addActionListener(e -> counter.decrement());
+        stop.addActionListener(e -> {
+            counter.stopCounting();
+            up.setEnabled(false);
+            down.setEnabled(false);
+            stop.setEnabled(false);
+        });
+    }
+
+    /**
+     * Counter agent that updates the display periodically.
+     */
+    private final class Counter implements Runnable {
+
+        private volatile boolean stop;
+        private volatile boolean isIncrement = true;
+        private int value;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    final var nextText = Integer.toString(this.value);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+
+                    if (this.isIncrement) {
+                        this.value++;
+                    } else {
+                        this.value--;
+                    }
+
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace(); // NOPMD: this is just an example
+                }
+            }
+        }
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void increment() {
+            this.isIncrement = true;
+        }
+
+        public void decrement() {
+            this.isIncrement = false;
+        }
+    }
 }
