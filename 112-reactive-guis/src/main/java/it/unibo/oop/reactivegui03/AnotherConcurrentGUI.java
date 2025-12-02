9159a88cf6bd5cae1b9a9ebaa56885e4e4d13bca
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-12-02 13:15:03 +0100

    Enhance AnotherConcurrentGUI with stop functionality and improve counter updates

--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -1,7 +1,15 @@
 package it.unibo.oop.reactivegui03;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import it.unibo.oop.JFrameUtil;
+
 import java.io.Serial;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Third experiment with reactive gui.
@@ -10,4 +18,96 @@ public final class AnotherConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+
+    private static final int TIME_SLEEP = 10_000;
+    private final Counter counter = new Counter();
+    private final Stopper stopper = new Stopper();
+    private final JLabel display = new JLabel();
+
+    /**
+     * Builds a new CGUI.
+     */
+    public AnotherConcurrentGUI() {
+        super();
+        JFrameUtil.dimensionJFrame(this);
+
+        final JPanel panel = new JPanel();
+        final JButton up = new JButton("up");
+        final JButton down = new JButton("down");
+        final JButton stop = new JButton("stop");
+
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+
+        new Thread(counter).start();
+        new Thread(stopper).start();
+
+        up.addActionListener(e -> counter.increment());
+        down.addActionListener(e -> counter.decrement());
+        stop.addActionListener(e -> {
+            counter.stopCounting();
+            up.setEnabled(false);
+            down.setEnabled(false);
+            stop.setEnabled(false);
+        });
+    }
+
+    /**
+     * Counter agent that updates the display periodically.
+     */
+    private final class Counter implements Runnable {
+
+        private volatile boolean stop;
+        private volatile boolean isIncrement = true;
+        private int value;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    final var nextText = Integer.toString(this.value);
+                    SwingUtilities.invokeAndWait(() -> AnotherConcurrentGUI.this.display.setText(nextText));
+
+                    if (this.isIncrement) {
+                        this.value++;
+                    } else {
+                        this.value--;
+                    }
+
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace(); // NOPMD: this is just an example
+                }
+            }
+        }
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void increment() {
+            this.isIncrement = true;
+        }
+
+        public void decrement() {
+            this.isIncrement = false;
+        }
+    }
+
+    private final class Stopper implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(TIME_SLEEP);
+                counter.stopCounting();
+            } catch (final InterruptedException e) {
+                e.printStackTrace(); // NOPMD: this is just an example
+            }
+        }
+    }
 }
